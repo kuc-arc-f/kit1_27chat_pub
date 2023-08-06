dba06a517586c445f4dede0096e5f07523dd50e9
中島浩二 <knaka0209@gmail.com>
2023-08-06 11:52:19 +0900

    up, 0806a7

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,20 @@ import Navi from './Navi.svelte';
 </script>
 
 <Navi />
-<div class="app">
+<div class="app app_root_wrap bg-white">
 	<main>
 		<slot />
 	</main>
-	<footer>
-	</footer>
+
 </div>
 
+<!-- CSS -->
 <style>
+.app_root_wrap{ margin-top: 63px; }
 </style>
 
 <!--
+<footer>
+</footer>
 <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 -->
